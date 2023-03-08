f1208b6ccae90f8c042878b97f45851a3ba56fc5
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-08 14:46:47 -0600

    array exercise done

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,3 +4,12 @@
 
 Console.WriteLine("Hello, Arrays!");
 
+int[] numArray = { 1, 1, 2, 3, 5, 8 };
+
+foreach (int num in numArray)
+{
+    if (num % 2 > 0)
+    {
+        Console.WriteLine(num);
+    }
+}
