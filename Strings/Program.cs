c64855cf235a6df2f866f32823080e80e888331b
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-08 14:47:36 -0600

    strings exercise done

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -4,3 +4,12 @@
 
 Console.WriteLine("Hello, Strings!");
 
+string greenEggs = "I would not, could not, in a box. I would not, could not with a fox.\r\nI will not eat them in a house. I will not eat them with a mouse.";
+
+string[] andHams = greenEggs.Split(" ");
+
+Console.WriteLine(string.Join(",", andHams));
+
+string[] sentEggs = greenEggs.Split(".");
+
+Console.WriteLine(string.Join(".", sentEggs));
