6d1d30b34075417d4003817c4de96291cfea445f
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-08 14:57:05 -0600

    dictionary exercises done

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -4,3 +4,30 @@
 
 Console.WriteLine("Hello, Dictionary!");
 
+Dictionary<double, string> students = new Dictionary<double, string>();
+string studentName;
+
+Console.WriteLine("Enter student name Enter to finish.");
+int studentId = 1;
+do
+{
+    Console.WriteLine("Student: ");
+    string input = Console.ReadLine();
+    studentName = input;
+
+    if (!Equals(studentName, ""))
+    {
+        students.Add(studentId, studentName);
+        studentId++;
+    }
+
+} while (!Equals(studentName, ""));
+
+// Print class roster
+Console.WriteLine("\nClass roster:");
+
+foreach (KeyValuePair<double, string> student in students)
+{
+    Console.WriteLine($"\nStudent name: {student.Value}");
+    Console.WriteLine($"Student ID: {student.Key}");
+}
