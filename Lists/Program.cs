d8ffd958d4eb144c92b3c46326ab59981abecc63
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-08 14:55:46 -0600

    list exercise done

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -3,3 +3,40 @@
 //TODO: Write your code for List Practice below:
 
 Console.WriteLine("Hello, Lists!");
+
+
+using System.Linq;
+
+static double findSum(List<double> numbers)
+{
+    double sum = 0;
+    foreach (double number in numbers)
+    {
+        if (number % 2 == 0)
+        {
+            sum += number;
+
+        }
+    }
+    return sum;
+}
+
+List<double> numList = new() {1,3,7,1,88,54,33,7,12,7,9};
+
+Console.WriteLine(findSum(numList));
+
+String seuss = "I would not could not in a box I would not could not with a fox I will not eat them in a house I will not eat them with a mouse";
+
+List<string> eggs;
+eggs = seuss.Split().ToList();
+
+Console.WriteLine("Enter a word length: ");
+int wordLength = int.Parse(Console.ReadLine());
+
+foreach (string word in eggs)
+{
+    if (word.Length == wordLength)
+    {
+        Console.WriteLine(word);
+    }
+}
