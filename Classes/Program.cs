ee777eb14dac1980ed864c6e92773d7392213ac2
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-08 23:10:18 -0600

    exercise first attempt

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,1 +1,8 @@
 ﻿// TODO: Instantiate your objects and test your exercise solutions with print statements here.
+using Classes;
+
+Student student = new Student("Charlie");
+
+Console.WriteLine(student.Name);
+student.Name = "Rocko";
+Console.WriteLine(student.Name);
--- Classes/Student.cs
+++ Classes/Student.cs
@@ -4,8 +4,26 @@ namespace Classes;
 
 public class Student
 {
-    private string Name;
-    private int StudentId;
-    private int NumberOfCredits;
-    private double Gpa;
+    private static int nextId;
+    public string Name { get; set; }
+    public int StudentId { get; set; }
+    public int NumberOfCredits { get; set; }
+    public double Gpa { get; set; }
+
+    public Student (string name, int studentId, int numberOfCredits, double gpa)
+    {
+        Name = name;
+        StudentId = studentId;
+        NumberOfCredits = numberOfCredits;
+        Gpa = gpa;
+    }
+
+    public Student(string name, int studentId): this(name, studentId, 0, 0.0)
+    {}
+
+    public Student(string name): this(name, nextId)
+    {
+        nextId++;
+    }
+
 }
