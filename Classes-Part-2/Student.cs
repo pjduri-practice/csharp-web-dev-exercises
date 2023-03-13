f59563c4a7e6b06d6009c50edaa48a4af929282f
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-13 11:31:20 -0500

    second go

--- Classes-Part-2/Student.cs
+++ Classes-Part-2/Student.cs
@@ -31,20 +31,50 @@ namespace Classes_Part_2
         public void AddGrade(int courseCredits, double grade)
         {
             // Update the appropriate properties: NumberOfCredits, Gpa
+            double totalQualityScore = Gpa * NumberOfCredits;
+            totalQualityScore += courseCredits * grade;
+            NumberOfCredits += courseCredits;
+            Gpa = totalQualityScore / NumberOfCredits;
         }
 
         //TODO: Complete the GetGradeLevel method here:
-        public string GetGradeLevel(int credits)
+        public string GetGradeLevel()
         {
             // Determine the grade level of the student based on NumberOfCredits
-            return "grade level tbd";
+            if (NumberOfCredits <= 29)
+            {
+                return "Freshman";
+            }
+            else if (NumberOfCredits <= 59)
+            {
+                return "Sophomore";
+            }
+            else
+            {
+                return "Senior";
+            }
         }
 
         // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
         //  than just the class fields.
+        public override string? ToString()
+        {
+            return $"{Name} (Credits: {NumberOfCredits} GPA: {Gpa})";
+        }
 
         // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
         //  Student objects equal.
+        public override bool Equals(object? obj)
+        {
+            return obj is Student student &&
+                   StudentId == student.StudentId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(StudentId);
+        }
+
     }
 }
 
