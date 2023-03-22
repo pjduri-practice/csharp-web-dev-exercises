778f29b41242e37af844a5fbec3bba4477474d33
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-22 13:49:17 -0500

    threw some exceptions

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,23 +1,47 @@
 ﻿// --------- Divide By Zero! ----------
 
-/* 
 static double Divide(double x, double y)
 {
     // Write your code here!
+    //try
+    //{
+    //    return x / y;
+    //} catch (ArgumentOutOfRangeException e)
+    //{
+    //    throw new ArgumentOutOfRangeException("You cannot divide by zero!");
+    //}
+
+    if(y == 0)
+    {
+        throw new ArgumentOutOfRangeException("You cannot divide by zero!");
+    }
+
+    return x / y;
 }
-*/
 
 // Test out your Divide() function here!
+//Console.WriteLine(Divide(7, 0));
 
 
 // --------- Test Student Labs ----------
 
-/*
 static int CheckFileExtension(string fileName)
 {
     // Write your code here!
+    if (fileName == null || fileName == "")
+    {
+        throw new ArgumentNullException("fileName", "Student did not submit work.");
+    }
+
+    if (fileName.EndsWith(".cs"))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
-*/
 
 // Test out your CheckFileExtension() function here!
 
@@ -26,5 +50,5 @@ students.Add("Carl", "Program.cs");
 students.Add("Brad", "");
 students.Add("Elizabeth", "MyCode.cs");
 
-
+Console.WriteLine(CheckFileExtension(students["Brad"]));
 
